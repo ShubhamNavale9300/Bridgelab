78e1ee1a238157b6262c693cd61313816f395ca9
Shubham Navale <shubhamnvale9300@gmail.com>
2026-08-08 15:22:45 +0530

    Celsius to Fahrenhit code

--- Variable.java
+++ Variable.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter ");
+        double celsius = sc.nextDouble();
+        double fahrenheit = (celsius *9/5)+32;
+        int intPart = (int) fahrenheit;
+        int roundedPart = (int) Math.round(fahrenheit);
+
+        System.out.println("Fahrenheit (double) = " + fahrenheit);
+        System.out.println("Integer part = " + intPart);
+        System.out.println("Rounded integer = " + roundedPart);
+
+        sc.close();
+
             System.out.println("hi");
              
     }
